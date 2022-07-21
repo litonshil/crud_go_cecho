6aa9d4539fecad0b5e82cca63408264acf3d4fe1
litonshil <litonshil009@gmail.com>
2022-07-21 14:51:27 +0600

    sending email after registration is complete

--- pkg/controllers/user-controller.go
+++ pkg/controllers/user-controller.go
@@ -8,10 +8,13 @@ import (
 	"github.com/litonshil/crud_go_echo/pkg/database"
 	"github.com/litonshil/crud_go_echo/pkg/models"
 	"github.com/litonshil/crud_go_echo/pkg/repository"
+	"github.com/litonshil/crud_go_echo/pkg/utils"
 )
 
 var db = database.GetDB()
 
+
+// Registration create a user
 func Registration(c echo.Context) error {
 	var user = new(models.User)
 
@@ -22,9 +25,16 @@ func Registration(c echo.Context) error {
 	if err := repository.CreateUser(user); err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
+
+	// Send username and password via email
+	if err := utils.SendEmail(user); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
 	return c.JSON(http.StatusCreated, "user created successfullys")
 }
 
+// GetAllUsers fetch all user 
 func GetAllUsers(c echo.Context) error {
 
 	res, err := repository.GetAllUsers()
@@ -34,6 +44,7 @@ func GetAllUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+// GetAUsers fetch an specific user based on id
 func GetAUsers(c echo.Context) error {
 	id := c.Param("id")
 	user_id, _ := strconv.Atoi(id)
@@ -44,8 +55,9 @@ func GetAUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-func checkEmptyUserField(user *models.User,old_user *models.User) *models.User {
-	if (user.Name == "") {
+// checkEmptyUserField set all empty field with old data when an user is update
+func checkEmptyUserField(user *models.User, old_user *models.User) *models.User {
+	if user.Name == "" {
 		user.Name = old_user.Name
 	}
 	if user.Address == "" {
@@ -63,6 +75,7 @@ func checkEmptyUserField(user *models.User,old_user *models.User) *models.User {
 	return user
 }
 
+// UpdateUser update an user
 func UpdateUser(c echo.Context) error {
 
 	var user = new(models.User)
@@ -71,7 +84,6 @@ func UpdateUser(c echo.Context) error {
 	if err := c.Bind(user); err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
-
 
 	id := c.Param("id")
 
@@ -85,7 +97,7 @@ func UpdateUser(c echo.Context) error {
 
 	user.Id = user_id
 
-	checkedUser := checkEmptyUserField(user,old_user)
+	checkedUser := checkEmptyUserField(user, old_user)
 
 	res, err := repository.UpdateUser(user_id, checkedUser)
 	if err != nil {
@@ -94,6 +106,7 @@ func UpdateUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+// DeleteUser delete an user
 func DeleteUser(c echo.Context) error {
 	id := c.Param("id")
 	user_id, _ := strconv.Atoi(id)
